7cd027330ff20903a0c77c500c581b7ddb9aa934
pcion <patrick.ciongoli@gmail.com>
2022-04-29 00:41:01 -0400

    added backend reply functionality

--- routes/api/comment-routes.js
+++ routes/api/comment-routes.js
@@ -2,12 +2,16 @@ const router = require("express").Router();
 const {
    addComment,
    removeComment,
+   addReply,
+   removeReply,
 } = require("../../controllers/comment-controller");
 
 // /api/comments/<pizzaId>
 router.route("/:pizzaId").post(addComment);
+// route method for deleting reply's
+router.route("/:pizzaId/:commentId/:replyId").delete(removeReply);
 
 // /api/comments/<pizzaId>/<commentId>
-router.route("/:pizzaId/:commentId").delete(removeComment);
+router.route("/:pizzaId/:commentId").put(addReply).delete(removeComment);
 
 module.exports = router;
